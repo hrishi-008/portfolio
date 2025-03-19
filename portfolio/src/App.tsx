7deba3f6087d6e386b4f66ad6a6ce9321ba834d9
Hrishikesh Kalola <97169623+hrishi-008@users.noreply.github.com>
2025-03-19 12:44:09 +0530

    Add analytics

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from "@vercel/analytics/react";
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -23,6 +24,7 @@ const App: React.FC = () => {
           <Contact />
         </main>
       </div>
+      <Analytics />
     </div>
   );
 };
