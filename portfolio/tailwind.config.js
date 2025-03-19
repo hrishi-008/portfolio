8e3423b5fa9d5db234837b47cf34972ebd1aae20
Hrishikesh Kalola <97169623+hrishi-008@users.noreply.github.com>
2025-03-19 11:11:44 +0530

    change config files

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+import defaultTheme from 'tailwindcss/defaultTheme'
 
-module.exports = {
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
